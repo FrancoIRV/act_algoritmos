a427584d28bb27da72afee648a5fb455cc53c2f6
francoIRV <f.rivas09@ufromail.cl>
2026-03-18 21:50:17 -0300

    subiendo act 3 de algoritmos. funciona en algunos casos pero falta arreglar el bucle

--- Prueba3.py
+++ Prueba3.py
@@ -1,1 +1,16 @@
-print("prueba3")
+def bubble(lst):
+    lst_picks = []
+    listaOrdenada = False
+    while listaOrdenada == False:
+        for i in range (0, len(lst) -1):
+            if lst[i] > lst[i+1]:
+                numMenor = lst[i+1]
+                lst[i+1 ] = lst[i]
+                lst[i] = numMenor
+                lst_picks.append(lst)
+            else:
+                listaOrdenada = True
+    return lst_picks
+
+prueba = [3,2,1, 5, 6]
+print(bubble(prueba))
