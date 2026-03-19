5a126fc1d1b9a064cfb51a00a0821d597d36450b
francoIRV <f.rivas09@ufromail.cl>
2026-03-18 23:40:57 -0300

    version finalizada del ejercicio que paso todas las pruebas.

--- Prueba3.py
+++ Prueba3.py
@@ -1,15 +1,14 @@
 def bubble(lst):
     lst_picks = []
-    listaOrdenada = False
-    while listaOrdenada == False:
-        for i in range (0, len(lst) -1):
+    loops = len(lst) -1
+    while  loops > 1 :
+        for i in range (loops):
             if lst[i] > lst[i+1]:
                 numMenor = lst[i+1]
-                lst[i+1 ] = lst[i]
+                lst[i+1] = lst[i]
                 lst[i] = numMenor
-                lst_picks.append(lst)
-            else:
-                listaOrdenada = True
+                lst_picks.append(lst.copy())
+        loops = loops -1
     return lst_picks
 
 prueba = [3,2,1, 5, 6]
